2db001ae1966beab266c25abbf092cbe9a0dc43c
ismailalpergenc <147383870+ismailalpergenc@users.noreply.github.com>
2024-01-02 18:52:53 +0300

    Update deneme.c
    En yuksek skoru dosyadan okuma, yazma gibi ozellikler ekledim.
    "enskor.txt" dosyasının formatlaması asagıdaki gibi olmalıdır:
    
    7
    Alper

--- deneme.c
+++ deneme.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 #define SORU_SAYISI 10
 #define SORU_MAKS_UZUNLUK 256
-
-char userisim[21];
+int enYuksekskor;
+char userisim[21],enisim[21];
 char soruSor(char *soru) {
     char cevap;
     printf("%s\n", soru);
@@ -16,11 +16,11 @@ char soruSor(char *soru) {
 
 int cevapKontrol(char kullaniciCevap, char dogruCevap, int skor) {
     if (kullaniciCevap == dogruCevap) {
-        printf("Dogru!\n\n");
+        printf("Dogru!\n-------------------------------------------------------------\n");
         return skor + 1;
     }
     else {
-        printf("Yanlis. Dogru Cevap: %c.\n\n", dogruCevap);
+        printf("Yanlis. Dogru Cevap: %c.\n-------------------------------------------------------------\n", dogruCevap);
         return skor;
     }
 }
@@ -37,24 +37,37 @@ void quizBaslat(FILE *soruDosya, FILE *skorDosya) {
         skor = cevapKontrol(kullaniciCevap, dogruCevap, skor);
     }
     printf("Skorunuz: %d\n", skor);
-    fprintf(skorDosya,"%s Kullanicisinin Skoru: %d\n",userisim,skor);
+    fprintf(skorDosya,"%s Kullanicisinin Skoru: %d\n",userisim,skor);
+    if (skor > enYuksekskor){
+        printf("Tebrikler, artik en yuksek skorun sahibisiniz.");
+        FILE *enskorDosya = fopen("enskor.txt","w");
+        enYuksekskor=skor;
+        fprintf(enskorDosya,"%d\n%s",skor,userisim);
+        fclose(enskorDosya);
+    }
+    else{}
 }
 
 
 int main() {
     FILE *soruDosya = fopen("sorular.txt","r"); //Soru dosyasını okuma modunda olucak sekilde acar
     FILE *skorDosya = fopen("skor.txt","a"); //Skor dosyasını append modunda olucak sekilde acar
+    FILE *enskorDosya = fopen("enskor.txt","r");
     if (soruDosya == NULL || skorDosya == NULL) {
         printf("Hata: Dosya acilirken bir sorun olustu.\n");
         return 1; //Error verip cikar
     }
+    fscanf(enskorDosya,"%d ",&enYuksekskor);
+    fgets(enisim,sizeof(enisim),enskorDosya);
+    fgetc(enskorDosya);
     printf("\n-- Quiz Proje Uygulamasi --\n");
+    printf("\nEn Yuksek Skor %s Kullanicisinin Skoru:%d\n\n",enisim,enYuksekskor);
     printf("Isminizi giriniz: \n");
     scanf(" %s",&userisim);
-    //printf("\n ! Quizde yapilan en yuksek skor %d'dir ! \n",enYuksekSkor);
     //Beyler Burda Skor dosyasında bi highscore değişkeni oluşturup onu yazdıralım.
     quizBaslat(soruDosya, skorDosya);
     fclose(soruDosya); //Soru dosyasını kapatır
     fclose(skorDosya); //Skor dosyasını kapatır
+    fclose(enskorDosya);
     return 0;
 }

3a976d61c8c855038620f2d4b72b5a806fbd83bb
İsmail Alper Genç <147383870+ismailalpergenc@users.noreply.github.com>
2024-01-03 13:06:20 +0300

    Update soruSor.c
    kod acıklamaları

--- soruSor.c
+++ soruSor.c
@@ -1,7 +1,8 @@
 
-#include <stdio.h>
-#include "quiz.h"
+#include <stdio.h> //Standart C Kütüphanesi
+#include "quiz.h" //Fonksiyonları kendi dosyalarından çekmek için oluşturduğumuz Header'ı tanımladığımız satır
 
+//Soruyu ekrana yazıp kullanıcının cevabını alan fonksiyon
 char soruSor(char *soru) {
     char cevap;
     printf("%s\n", soru);
